cf9702c0e707e469f4d9d0860105a7bf98070611
Sally <sallychau1108@gmail.com>
2018-04-12 17:32:15 +0800

    Moved start study buttons to SelectStudyPage and added language selection option.

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { AlertController, LoadingController, NavController, NavParams, ToastController } from 'ionic-angular';
-import { StudyPhasePage } from '../studyPhase/studyPhase'
 import { VocabProvider } from "../../providers/vocab/vocab";
-import { VocabularyListPage } from "../vocabularyList/vocabularyList";
 import 'rxjs/add/operator/map';
-import { Card } from '../../../model/card';
-import { OverviewPage } from "../overview/overview";
+import {SelectStudyPage} from "../selectStudy/selectStudy";
 
 @Component({
   selector: 'page-home',
@@ -24,67 +21,16 @@ export class HomePage {
 
     }
 
+  protected  language:String;
 
-
-  startLearning() {
-    let curCards: Card[] = this.vocabProvider.getCardsToLearn();
-
-    if (curCards.length > 0) {
-      this.navCtrl.setRoot(StudyPhasePage, {
-        cards: curCards
-      });
-    } else {
-      this.toastCtrl.create({
-        message: 'No cards for learning left! Try again later.',
-        duration: 3000,
-        position: 'bottom'
-      }).present();
+  start() {
+    switch (this.language) {
+      case 'Spanish':
+        this.navCtrl.setRoot(SelectStudyPage, {
+          language: this.language
+        });
+        break;
     }
-  }
-
-  createCard() {
-    let prompt = this.alertCtrl.create({
-      title: 'Create new card',
-      message: "Enter a new vocabulary!",
-      inputs: [
-        {
-          name: 'front',
-          placeholder: 'Front'
-        },
-        {
-          name: 'back',
-          placeholder: 'Back'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Save',
-          handler: data => {
-            this.vocabProvider.createCard(data.front, data.back);
-          }
-        }
-      ]
-    });
-
-    prompt.present();
-  }
-
-  resetAll() {
-    this.vocabProvider.clearStorage();
-  }
-
-  public loadCsvToDict() {
-    this.vocabProvider.addTenVocs();
-  }
-
-  showProgress() {
-    this.navCtrl.setRoot(OverviewPage);
   }
 
 }
