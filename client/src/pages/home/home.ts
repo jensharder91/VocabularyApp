9ac870f23011e531e9cf98c307e8c1ce13c6fbed
JENS\Jens <je.harder@gmail.com>
2018-04-14 14:22:59 +0200

    home page language selection

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AlertController, LoadingController, NavController, NavParams, ToastController } from 'ionic-angular';
 import { VocabProvider } from "../../providers/vocab/vocab";
 import 'rxjs/add/operator/map';
-import {SelectStudyPage} from "../selectStudy/selectStudy";
+import { SelectStudyPage } from "../selectStudy/selectStudy";
 
 @Component({
   selector: 'page-home',
@@ -12,6 +12,13 @@ import {SelectStudyPage} from "../selectStudy/selectStudy";
 
 export class HomePage {
 
+  private languages = [
+    { key: "English", displayName: "ENG", "displayImage": "gb.svg" },
+    { key: "Spanish", displayName: "ESP", "displayImage": "spain.svg" },
+    { key: "Test", displayName: "TEST", "displayImage": "" },
+    { key: "German", displayName: "DEU", "displayImage": "german.svg" }
+  ];
+
   constructor(public alertCtrl: AlertController,
     public navCtrl: NavController,
     private toastCtrl: ToastController,
@@ -19,18 +26,13 @@ export class HomePage {
     public loadingCtrl: LoadingController,
     public vocabProvider: VocabProvider) {
 
-    }
-
-  protected  language:String;
-
-  start() {
-    switch (this.language) {
-      case 'Spanish':
-        this.navCtrl.setRoot(SelectStudyPage, {
-          language: this.language
-        });
-        break;
-    }
   }
 
+  protected language: String;
+
+  start(key: string) {
+    this.navCtrl.setRoot(SelectStudyPage, {
+      language: key
+    });
+  }
 }
